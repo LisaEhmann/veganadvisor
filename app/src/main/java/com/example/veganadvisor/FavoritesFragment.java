c1bdd050cad73ff6d67d7670bf72ca2c800af578
LisaEhmann <65292175+LisaEhmann@users.noreply.github.com>
2020-06-12 11:25:25 +0200

    content.xml erstellt. drawable contents erstellt.

--- app/src/main/java/com/example/veganadvisor/FavoritesFragment.java
+++ app/src/main/java/com/example/veganadvisor/FavoritesFragment.java
@@ -6,11 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,16 +22,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class FavoritesFragment extends Fragment {
+    //Objekte instanziieren
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Nullable
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         DatabaseReference myRef = database.getReference().child("restaurant").child("id1").child("adress");
-
         View v = inflater.inflate(R.layout.fragment_favoriten, container, false);
+
+
         final TextView textView = (TextView)v.findViewById(R.id.example);
 
         myRef.addValueEventListener(new ValueEventListener() {
@@ -42,6 +49,9 @@ public class FavoritesFragment extends Fragment {
         });
         return v;
     }
-
     }
+
+
+
+
 
